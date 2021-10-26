ff27305e7f6d926c1fab26bb04167d25dd681b2a
Ho-Lee <ho.sol.lee@gmail.com>
2021-10-26 22:02:33 +0900

    feat: 자동차 이름을 입력 시 최소 두대의 차를 입력하도록 유효성 검사를 진행하는 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,7 +9,16 @@ public class RacingGame {
 
 	public void gameStart() {
 		String carNamesString = InputView.getCarsNames();
+		validateCarsName(carNamesString);
+
 		int attempt = InputView.getRacingAttempt();
+	}
+
+
+	private void validateCarsName(String carNames) {
+		if(isLessThanTwo(carNames)){
+			throw new IllegalArgumentException("자동차는 최소 두대 이상이여야 합니다.");
+		}
 	}
 
 	private boolean isLessThanTwo(String carNames) {
